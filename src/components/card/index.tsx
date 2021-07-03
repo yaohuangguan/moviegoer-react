a152860d1a69fb9fb458eeed88f15f04c41f74ca
SamYao <yaob@miamioh.edu>
2021-07-03 21:14:57 +0800

    add

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,30 +7,39 @@ import {
 } from "@ant-design/icons";
 const { Meta } = Card;
 
+interface ICard {
+  data: any[];
+  loading: boolean;
+}
 
-const CardComponent = (props) => {
-  const { data, loading, style } = props;
+const CardComponent = (props: ICard) => {
+  const { data, loading } = props;
 
-  return data.map((card) => (
-    <Card
-      style={{ width: '100%', marginTop: 16 }}
-      actions={[
-        <SettingOutlined key="setting" />,
-        <EditOutlined key="edit" />,
-        <EllipsisOutlined key="ellipsis" />,
-      ]}
-    >
-      <Skeleton loading={loading} avatar active>
-        <Meta
-          avatar={
-            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-          }
-          title={card.title}
-          description={card.description}
-        />
-      </Skeleton>
-    </Card>
-  ));
+  return (
+    <>
+      {data.map((card) => (
+        <Card
+          key={card.description}
+          style={{ width: "100%", marginTop: 16 }}
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
+        >
+          <Skeleton loading={loading} avatar active>
+            <Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title={card.title}
+              description={card.description}
+            />
+          </Skeleton>
+        </Card>
+      ))}
+    </>
+  );
 };
 
 export default CardComponent;
